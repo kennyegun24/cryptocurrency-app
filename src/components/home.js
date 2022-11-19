d3ea88674e10224da9b659de438c0c6c9d080ec8
kenny Egun <kennyegun240@gmail.com>
2022-11-19 22:13:32 +0100

    Add class

--- src/components/home.js
+++ src/components/home.js
@@ -11,21 +11,20 @@ const Home = () => {
     dispatch(fetchCoins());
   }, [dispatch]);
   return (
-    <div className="flex jstCnt aliCnt flxWrp gap10px">
+    <div className="flex jstCnt flxWrp aliCnt ">
       {coins.map((coins) => (
-        <div>
-        <Display
-          key={coins.id.id}
-          img={coins.id.image}
-          id={coins.id.id.toUpperCase()}
-          name={coins.id.name}
-          symbol={coins.id.symbol.toUpperCase()}
-          atl={coins.id.atl}
-          price={coins.id.current_price}
-        />
-        <NavLink state={coins} to="/display">
-          <h2>{coins.id.ath}</h2>
-        </NavLink>
+        <div className="flxCnt flex jstCnt aliCnt flxWrp gap10px sec1" key={coins.id.id}>
+          <NavLink className="coinLink" state={coins} to="/display">
+            <Display
+              key={coins.id.id}
+              img={coins.id.image}
+              id={coins.id.id.toUpperCase()}
+              name={coins.id.name}
+              symbol={coins.id.symbol.toUpperCase()}
+              atl={coins.id.atl}
+              price={coins.id.current_price}
+            />
+          </NavLink>
         </div>
       ))}
     </div>
