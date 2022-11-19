69c23adbd36e9cdb2e48027f4b1cc80bce6561cc
kenny Egun <kennyegun240@gmail.com>
2022-11-19 22:14:01 +0100

    Remove nav menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="nav">
+    <div className="navDiv">
+      <Link className="link" to="/">&times;</Link>
+    </div>
     <div>
       <h3 className="header">Cryptocurrency Hub</h3>
     </div>
-    <div className="navDiv">
-      <Link className="link" to="/">Home</Link>
-      <Link className="link" to="/display">Display</Link>
-    </div>
+
   </nav>
 );
 
