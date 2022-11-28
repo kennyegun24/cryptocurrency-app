27f2823f94279124dd721034763b218ebc3e89c6
kenny Egun <kennyegun240@gmail.com>
2022-11-28 22:43:38 +0100

    Implement search function

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,62 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 import { fetchCoins } from '../redux/coin/coin';
 import Display from './display';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const coins = useSelector((state) => state.coins);
+
+  const params = useParams();
+  const { coins, status } = useSelector((state) => state.coins);
+  const [search, setSearch] = useState('');
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`;
   useEffect(() => {
-    dispatch(fetchCoins());
-  }, [dispatch]);
+    if (status === null) {
+      dispatch(fetchCoins(url));
+    }
+  }, [params.coinId, dispatch]);
+
   return (
-    <div className="flex jstCnt flxWrp aliCnt ">
-      {coins.map((coins) => (
-        <div className="flxCnt flex jstCnt aliCnt flxWrp gap10px sec1" key={coins.id.id}>
-          <NavLink className="coinLink" state={coins} to="/display">
-            <Display
-              key={coins.id.id}
-              img={coins.id.image}
-              id={coins.id.id.toUpperCase()}
-              name={coins.id.name}
-              symbol={coins.id.symbol.toUpperCase()}
-              atl={coins.id.atl}
-              price={coins.id.current_price}
-            />
-          </NavLink>
+    <div>
+      {status === 'pending' ? (
+        <div className="pending">
+          <p className="round" />
+          <h3>LOADING...</h3>
         </div>
-      ))}
+      ) : (
+        <>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="search"
+          />
+          <FaSearch />
+
+          <div className="flex jstCnt flxWrp aliCnt ">
+            {coins.filter((searchCoin) => searchCoin.name.toLowerCase()
+              .includes(search.toLowerCase())
+              || searchCoin.symbol.toLowerCase().includes(search.toLowerCase()))
+              .map((coins) => (
+                <div className="flxCnt flex jstCnt aliCnt flxWrp gap10px sec1" key={coins.id}>
+                  <NavLink className="coinLink" state={coins} to="/display">
+                    <Display
+                      key={coins.id}
+                      img={coins.image}
+                      id={coins.id.toUpperCase()}
+                      name={coins.name}
+                      symbol={coins.symbol.toUpperCase()}
+                      atl={coins.atl}
+                      price={coins.current_price}
+                    />
+                  </NavLink>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
