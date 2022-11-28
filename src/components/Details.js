04a4c00544f3af0b9b1eb3d8c59b3665f4983354
kenny Egun <kennyegun240@gmail.com>
2022-11-28 22:43:10 +0100

    Modified

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,60 +1,61 @@
+/* eslint-disable */
 import { useLocation } from 'react-router-dom';
+import React from 'react';
 
 const Details = () => {
   const location = useLocation();
   const { state } = location;
-
   const total = () => {
-    if (state.id.total_supply == null) {
+    if (state.total_supply == null) {
       return <span>{`${state.id.total_supply}`}</span>;
     }
-    return <span>{`Total Supply: ${state.id.total_supply.toLocaleString()}`}</span>;
+    return <span>{`Total Supply: ${state.total_supply.toLocaleString()}`}</span>;
   };
   return (
     <div className="detailPage">
-      <img className="img" src={state.id.image} alt="img" />
+      <img className="img" src={state.image} alt="img" />
       <span>
-        {` ${state.id.name}`}
+        {` ${state.name}`}
       </span>
       <span>
-        {` ${state.id.symbol.toUpperCase()}`}
+        {` ${state.symbol.toUpperCase()}`}
       </span>
       <span>
         market cap:
-        {` ${Number(state.id.market_cap).toLocaleString('en-US')}`}
+        {` ${Number(state.market_cap).toLocaleString('en-US')}`}
       </span>
       <span>
         price:
-        {` ${Number(state.id.current_price).toLocaleString('en-US')}`}
+        {` ${Number(state.current_price).toLocaleString('en-US')}`}
       </span>
       <span>
         ATH:
-        {` ${state.id.ath.toLocaleString()}`}
+        {` ${state.ath.toLocaleString()}`}
       </span>
       {total()}
       <span>
         Circulating Supply:
-        {` ${state.id.circulating_supply.toLocaleString()}`}
+        {` ${state.circulating_supply.toLocaleString()}`}
       </span>
       <span>
         Total Volume:
-        {` ${state.id.total_volume.toLocaleString()}`}
+        {` ${state.total_volume.toLocaleString()}`}
       </span>
       <span>
         Cap Rank:
-        {` ${state.id.market_cap_rank.toLocaleString()}`}
+        {` ${state.market_cap_rank.toLocaleString()}`}
       </span>
       <span>
         High 24hr:
-        {` ${state.id.high_24h.toLocaleString()}`}
+        {` ${state.high_24h.toLocaleString()}`}
       </span>
       <span>
         Low 24hr:
-        {` ${state.id.low_24h.toLocaleString()}`}
+        {` ${state.low_24h.toLocaleString()}`}
       </span>
       <span>
         Price Change 24hr:
-        {` ${state.id.price_change_24h.toLocaleString()}`}
+        {` ${state.price_change_24h.toLocaleString()}`}
       </span>
     </div>
   );
