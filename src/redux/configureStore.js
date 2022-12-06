d9c548edb950f0f689d1cebcf9e454c3980c782d
BELINDA <kennyegun240@gmail.com>
2022-12-06 17:19:56 +0100

    Add details reducers ti store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import coinsSlice from './coin/coin';
+import coin from './coin/coinDetail';
 
 const allReducers = combineReducers({
   coins: coinsSlice,
+  coin,
 });
 
 const store = configureStore({ reducer: allReducers }, applyMiddleware(thunk));
